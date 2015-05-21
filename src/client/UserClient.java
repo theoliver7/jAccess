86e094a6fd1acb2639d2bb325ff100857ea5fd05
Oliver Aschwanden <Oliver.Aschwanden@bbcag.ch>
2015-05-21 10:40:16 +0200

    comiit

--- src/client/UserClient.java
+++ src/client/UserClient.java
@@ -79,7 +79,7 @@ public class UserClient {
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
-			System.out.println(server);
+			
 			serverobj = (UserIntf) Naming.lookup(server);
 		} catch (MalformedURLException | RemoteException | NotBoundException e) {
 			JOptionPane.showMessageDialog(null, "Der Server hat zurzeit Probleme! \nBitte wenden " + "Sie sich an den IT-Support.", "Fehler", JOptionPane.ERROR_MESSAGE);
@@ -138,10 +138,6 @@ public class UserClient {
 		frame.repaint();
 
 		frame.addWindowListener(new ExitListener(frame));
-<<<<<<< HEAD
-
-=======
->>>>>>> origin/master
 	}
 
 	/**
